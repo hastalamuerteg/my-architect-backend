a1c6ef966a5b8d5f6a003ba0396f281afe25a460
Gabriel Vicente <gabriel.vicent@hotmail.com>
2023-01-09 12:59:41 -0300

    added auth/verify user route + refacs

--- src/vendors/joi-validation/schemas/architects/createArchitectSchema.ts
+++ src/vendors/joi-validation/schemas/architects/createArchitectSchema.ts
@@ -4,17 +4,14 @@ const createArchitectSchema = joi.object({
   firstName: joi.string()
     .min(3)
     .max(20)
-    .pattern(/^[a-zA-Z]{3,30}$/)
     .required(),
   lastName: joi.string()
     .min(5)
     .max(30)
-    .pattern(/^[a-zA-Z]{3,30}$/)
     .required(),
   gender: joi.string()
     .min(3)
     .max(30)
-    .pattern(/^[a-zA-Z]{3,30}$/)
     .required(),
   phone: joi.string()
     .pattern(/(\(?\d{2}\)?\s)?(\d{4,5}\d{4})/)
@@ -26,7 +23,6 @@ const createArchitectSchema = joi.object({
     .trim()
     .required(),
   password: joi.string()
-    .pattern(/^[a-zA-Z0-9]{3,30}$/)
     .required(),
 });
 
